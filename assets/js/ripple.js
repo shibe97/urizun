57a584337e4f5ce3a2f572bf438086579ceb7aa4
Kazuki Shibata <k.shibata97@gmail.com>
2015-05-12 15:48:10 +0900

    complete

--- assets/js/ripple.js
+++ assets/js/ripple.js
@@ -1,11 +1,11 @@
 $(function(){
-    $('.js-ripple').ripples({
+    var $el = $('.js-ripple');
+    $el.ripples({
         resolution: 512,
         dropRadius: 20,
         perturbance: 0.04,
     });
     setInterval(function() {
-        var $el = $('.js-ripple');
         var x = Math.random() * $el.outerWidth();
         var y = Math.random() * $el.outerHeight();
         var dropRadius = 20;
@@ -13,15 +13,17 @@ $(function(){
         $el.ripples('drop', x, y, dropRadius, strength);
     }, 800);
     setTimeout(function(){
-        $('.js-ripple').addClass('ripple');
+        $el.addClass('ripple');
         setTimeout(function(){
-            $('.js-ripple').ripples('destroy');
+            $el.ripples('destroy');
+            $el.hide();
         }, 2000);
     }, 5000);
-    $('.js-ripple').on('click', function(){
-        $(this).addClass('ripple');
+    $el.on('click', function(){
+        $el.addClass('ripple');
         setTimeout(function(){
-            $('.js-ripple').ripples('destroy');
+            $el.ripples('destroy');
+            $el.hide();
         }, 2000);
     });
 });
